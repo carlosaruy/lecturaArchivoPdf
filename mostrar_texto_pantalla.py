d0fa35a9dfd1a3634e436e7685a0133f5811827b
Carlos Armstrong <carlosarmstrong@github.com>
2022-12-21 23:06:02 -0300

    argparser - recibe nombre de archivo

--- mostrar_texto_pantalla.py
+++ mostrar_texto_pantalla.py
@@ -1,9 +1,16 @@
 import sys
+import argparse
 import extract_pdf_text
 
 def main():
+    #parser para recibir para indicar como se utiliza el script, y solicitar el parámetro: nombre arcchivo
+    parser = argparse.ArgumentParser(description='Extrae el texto de un archivo PDF')
+    parser.add_argument('file', help='Ruta al archivo PDF a leer')
+    args = parser.parse_args()
+
+
     # Obtener el nombre del archivo del primer argumento del script
-    file_name = sys.argv[1]
+    file_name = args.file
 
     # Invocar la función extract_text_from_pdf con el nombre del archivo
     text = extract_pdf_text.extract_text_from_pdf(file_name)
